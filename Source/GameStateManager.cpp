6597be709ddd629bf9787c6d4cbb8a68ee7762da
tiaraum <m.tiarayasmeen@digipen.edu>
2026-01-31 21:41:36 +0800

    assets n new changes

---
+++ Source/GameStateManager.cpp
@@ -0,0 +1,69 @@
+#include "GameStateManager.h"
+#include "AEEngine.h"
+#include "Main.h"
+#include "Next.h"
+#include "Farm.h"
+
+
+// GSM state variables
+int current = 0, previous = 0, next = 0;
+
+// Function pointers for current state
+FP fpLoad = nullptr, fpInitialize = nullptr, fpUpdate = nullptr,
+fpDraw = nullptr, fpFree = nullptr, fpUnload = nullptr;
+
+void GSM_Initialize(int startingState) {
+    current = previous = next = startingState;
+
+    // Initialize function pointers for starting state
+    GSM_Update();
+}
+
+void GSM_Update() {
+    // Set function pointers based on current state
+    switch (current) {
+    case GS_MAIN_SCREEN:
+        fpLoad = MainScreen_Load;
+        fpInitialize = MainScreen_Initialize;
+        fpUpdate = MainScreen_Update;
+        fpDraw = MainScreen_Render;
+        fpFree = MainScreen_Free;
+        fpUnload = MainScreen_Unload;
+        break;
+
+    case GS_NEXT_SCREEN:
+        fpLoad = NextScreen_Load;
+        fpInitialize = NextScreen_Initialize;
+        fpUpdate = NextScreen_Update;
+        fpDraw = NextScreen_Render;
+        fpFree = NextScreen_Free;
+        fpUnload = NextScreen_Unload;
+        break;
+
+    case GS_FARM_SCREEN:
+        fpLoad = Farm_Load;
+        fpInitialize = Farm_Initialize;
+        fpUpdate = Farm_Update;
+        fpDraw = Farm_Render;
+        fpFree = Farm_Free;
+        fpUnload = Farm_Unload;
+        break;
+
+
+
+    case GS_EXIT:
+        // Clear function pointers for exit state
+        fpLoad = nullptr;
+        fpInitialize = nullptr;
+        fpUpdate = nullptr;
+        fpDraw = nullptr;
+        fpFree = nullptr;
+        fpUnload = nullptr;
+        break;
+    }
+
+    if (next != current) {
+        previous = current;
+        current = next;
+    }
+}
---
+++ Source/GameStateManager.h
@@ -0,0 +1,20 @@
+#pragma once
+
+enum GameState {
+    GS_MAIN_SCREEN = 0,
+    GS_FARM_SCREEN = 1,
+    GS_NEXT_SCREEN = 2,
+    GS_EXIT = 3
+};
+
+// Function pointer type
+typedef void(*FP)(void);
+
+// GSM state variables
+extern int current, previous, next;
+
+// Function pointers for current state
+extern FP fpLoad, fpInitialize, fpUpdate, fpDraw, fpFree, fpUnload;
+
+void GSM_Initialize(int startingState);
+void GSM_Update();
