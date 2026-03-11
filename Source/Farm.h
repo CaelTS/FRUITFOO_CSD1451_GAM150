e4e9d23b68156c56a3d0fa36d2540b6b9f10016c
CaelTS <tyerinova17@gmail.com>
2026-03-11 10:56:35 +0800

    copium fix

--- Source/Farm.h
+++ Source/Farm.h
@@ -8,8 +8,6 @@ void Farm_Render();
 void Farm_Free();
 void Farm_Unload();
 
-void Farm_PlantSeed(int seedType);
-
 
 struct Plot
 {
@@ -20,7 +18,6 @@ struct Plot
 
 void Farm_PlantSeed(int plotIndex, int seedType);
 bool Farm_IsPlotPlanted(int plotIndex);
-bool Farm_IsPlotPlanted(int index);
 void Farm_ClearPlot(int index);
 bool Farm_ShouldStartRhythm();
 int  Farm_GetRhythmPlotIndex();
