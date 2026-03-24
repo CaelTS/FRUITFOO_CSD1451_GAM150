1a4bcb5705d6864e51b1f259ae3e226d695cb4b2
tiaraum <m.tiarayasmeen@digipen.edu>
2026-03-25 00:55:52 +0800

    cleaned up code and fix bug where if you play from profile rhythm prompt wont appear

--- Source/Farm.h
+++ Source/Farm.h
@@ -1,5 +1,31 @@
 #pragma once
+#include <vector>
 
+// ------------------------------------------------------------
+// FARM DATA STRUCTURE (EXPOSED TO OTHER FILES)
+// ------------------------------------------------------------
+struct FarmPlot
+{
+    bool isUnlocked = false;
+
+    bool isPlanted = false;
+    bool isReady = false;
+    float growTimer = 0.0f;
+    int seedType = -1;
+
+    bool rhythmTriggered = false;
+    bool waitingForRhythm = false;
+    bool growthFrozen = false;
+};
+
+// ------------------------------------------------------------
+// GLOBAL ACCESS (FOR HELPER, UI, ETC)
+// ------------------------------------------------------------
+extern std::vector<FarmPlot> farmPlots;
+
+// ------------------------------------------------------------
+// CORE FARM FUNCTIONS
+// ------------------------------------------------------------
 void Farm_Load();
 void Farm_Initialize();
 void Farm_Update();
@@ -7,18 +33,17 @@ void Farm_Render();
 void Farm_Free();
 void Farm_Unload();
 
-struct Plot
-{
-    float x;
-    float y;
-    int plantedSeed;   // -1 = empty
-};
-
+// ------------------------------------------------------------
+// PLOT INTERACTION
+// ------------------------------------------------------------
 void Farm_PlantSeed(int plotIndex, int seedType);
 bool Farm_IsPlotPlanted(int plotIndex);
 void Farm_ClearPlot(int index);
 bool Farm_IsPlotLocked(int index);
 
+// ------------------------------------------------------------
+// RHYTHM SYSTEM
+// ------------------------------------------------------------
 bool Farm_ShouldStartRhythm();
 int  Farm_GetRhythmPlotIndex();
 void Farm_ClearRhythmRequest();
@@ -28,4 +53,4 @@ void Farm_OnRhythmResult(bool success);
 void Farm_SetRhythmPaused(bool paused);
 bool Farm_IsRhythmPaused();
 bool Farm_IsWaitingForRhythm();
-int  Farm_GetRhythmSeedType(); // seed type of the plot that triggered rhythm
+int  Farm_GetRhythmSeedType();
